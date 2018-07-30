6c16e87f4f5ce6857788a2fcb2a8b7100d8aa600
m2o2o2d <2540760318@qq.com>
2018-07-30 16:54:00 +0800

    Route for home,login,register

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import FastClick from 'fastclick';
+import { AppContainer } from 'react-hot-loader';
+import { Provider } from 'react-redux';
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
+import store from '@/store/store';
+import asyncComponent from '@/utils/asyncComponent';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+FastClick.attach(document.body);
+
+const home = asyncComponent(() => import("@/pages/home/home"));
+const login = asyncComponent(() => import("@/pages/login/login"));
+const register = asyncComponent(() => import("@/pages/login/register"));
+
+const render = Component => {
+	ReactDOM.render(
+		// Bind to redux, hot loader
+		<Provider store={store}>
+			<AppContainer>
+				<HashRouter>
+					<Switch>
+						<Route path="/" exact component={home} />
+						<Route path="/login" component={login} />
+						<Route path="/register" component={register} />
+						<Redirect to="/" />
+					</Switch>
+				</HashRouter>
+			</AppContainer>
+		</Provider>,
+		document.getElementById('root')
+	);
+};
+
+render();
+
+// Webpack Hot Module Replacement(HMR) API
+if(module.hot) {
+	module.hot.accept();
+}
+
 registerServiceWorker();
---
+++ src/pages/home/home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+
+	render() {
+		return (
+			<p>Home</p>
+		);
+	};
+}
+
+export default Home;
